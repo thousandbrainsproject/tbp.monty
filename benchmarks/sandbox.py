9b08b2b1666229d5ef9459082b9a9fb486e0e634
Scott Knudstrup <scottknudstrup@gmail.com>
2024-12-23 17:25:37 -0500

    Update sandbox.py

--- benchmarks/sandbox.py
+++ benchmarks/sandbox.py
@@ -7,40 +7,18 @@ from tbp.monty.frameworks.run import main  # noqa: E402
 
 main(all_configs=CONFIGS, experiments=["base_config_10distinctobj_surf_agent"])
 
-# import matplotlib.pyplot as plt
-# import numpy as np
+"""
+Unsupervised
+----------------------------------------------------------------------------------------
+"""
 
-# from tbp.monty.frameworks.environment_utils.transforms import DepthTo3DLocations
-
-# t = DepthTo3DLocations(
-#     agent_id="agent_id_0",
-#     sensor_ids=["patch", "view_finder"],
-#     resolutions=[[64, 64], [64, 64]],
-#     zooms=[1.0, 10.0],
-#     get_all_points=True,
+# from tbp.monty.frameworks.utils.logging_utils import (
+#     load_stats,
+#     print_unsupervised_stats,
 # )
 
-# depth = np.ones((64, 64))
-# depth[32, 32] = 0
-
-# observations = {
-#     "agent_id_0": {
-#         "patch": {
-#             "depth": np.random.rand(64, 64),
-#             "rgba": np.random.rand(64, 64, 4),
-#         },
-#         "view_finder": {
-#             "depth": depth,
-#             "rgba": np.random.rand(64, 64, 4),
-#         },
-#     },
-# }
-
-# obs = t(observations)
-# depth = obs["agent_id_0"]["view_finder"]["depth"]
-# sem = obs["agent_id_0"]["view_finder"]["semantic_3d"][:, 3].reshape(depth.shape)
-
-
-# plt.imshow(sem)
-# plt.colorbar()
-# plt.show()
+# exp_path = "/Users/sknudstrup/tbp/results/monty/projects/monty_runs/surf_agent_unsupervised_10distinctobj"
+# stats, _, _, _ = load_stats(
+#     exp_path, load_eval=False, load_detailed=False, load_models=False
+# )
+# print_unsupervised_stats(stats, 10)
