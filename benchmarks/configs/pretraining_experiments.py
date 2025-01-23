1df2087c38bfa4df7458ad4da2a47b1b5b0c8c9d
Tristan Slominski <tslominski@thousandbrains.org>
2025-01-23 10:27:54 -0600

    refactor: make_habitat_configs.py -> simulators/habitat/configs.py

--- benchmarks/configs/pretraining_experiments.py
+++ benchmarks/configs/pretraining_experiments.py
@@ -31,12 +31,6 @@ from tbp.monty.frameworks.config_utils.make_dataset_configs import (
     get_env_dataloader_per_object_by_idx,
     get_object_names_by_idx,
 )
-from tbp.monty.frameworks.config_utils.make_habitat_configs import (
-    FiveLMMountHabitatDatasetArgs,
-    PatchViewFinderMountHabitatDatasetArgs,
-    SurfaceViewFinderMontyWorldMountHabitatDatasetArgs,
-    SurfaceViewFinderMountHabitatDatasetArgs,
-)
 from tbp.monty.frameworks.config_utils.policy_setup_utils import (
     make_naive_scan_policy_config,
 )
