72cc2729e24e5f4469f2aa727041a8c1cd1f9814
Tristan Slominski <tslominski@thousandbrains.org>
2025-01-24 17:29:06 -0600

    refactor: benchmarks only load config categories for experiments to run

--- benchmarks/configs/__init__.py
+++ benchmarks/configs/__init__.py
@@ -7,14 +7,3 @@
 # Use of this source code is governed by the MIT
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
-
-from .monty_world_experiments import CONFIGS as MONTY_WORLD
-from .monty_world_habitat_experiments import CONFIGS as MONTY_WORLD_HABITAT
-from .pretraining_experiments import CONFIGS as PRETRAININGS
-from .ycb_experiments import CONFIGS as YCB
-
-CONFIGS = dict()
-CONFIGS.update(PRETRAININGS)
-CONFIGS.update(YCB)
-CONFIGS.update(MONTY_WORLD)
-CONFIGS.update(MONTY_WORLD_HABITAT)
