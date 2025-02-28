61920a4c6097851eb3110e5dc48965192edd7a17
Tristan Slominski <tslominski@thousandbrains.org>
2025-02-28 17:39:57 -0600

    build: tag version commit with version tag

--- .github/actions/version_updated/check_version_updated.sh
+++ .github/actions/version_updated/check_version_updated.sh
@@ -5,6 +5,8 @@ do
   echo $changed_file
   if [[ $changed_file == src/tbp/monty/__init__.py ]]; then
     echo "version_updated=true" >> $GITHUB_OUTPUT
+    version=$(grep '__version__\s*=\s*' src/tbp/monty/__init__.py | awk -F'"' '{print $2}')
+    echo "version=$version" >> $GITHUB_OUTPUT
     exit 0
   fi
 done < changed_files.txt
