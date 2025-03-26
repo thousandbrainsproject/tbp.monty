596bdffb4c12072bc6e1ec397904da2f6ec4546e
Scott Knudstrup <scottknudstrup@gmail.com>
2025-03-26 16:19:53 -0400

    Update state_test.py

--- tests/unit/frameworks/models/state_test.py
+++ tests/unit/frameworks/models/state_test.py
@@ -6,12 +6,13 @@
 # Use of this source code is governed by the MIT
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
-
+import json
 import unittest
 
 import numpy as np
 from scipy.spatial.transform import Rotation
 
+from tbp.monty.frameworks.models.buffer import BufferEncoder
 from tbp.monty.frameworks.models.states import GoalState, encode_goal_state
 
 
@@ -52,8 +53,14 @@ class EncodeGoalStateTest(unittest.TestCase):
         self.goal_state = GoalState(**self.goal_state_dict)
 
     def test_encode(self):
-        encoded = encode_goal_state(self.goal_state)
-        self.assertDictEqual(encoded, self.goal_state_dict)
+        dct = encode_goal_state(self.goal_state)
+        self.assertDictEqual(dct, self.goal_state_dict)
+
+    def test_json_serialization(self):
+        self.assertDictEqual(
+            json.loads(json.dumps(self.goal_state, cls=BufferEncoder)),
+            json.loads(json.dumps(self.goal_state_dict, cls=BufferEncoder)),
+        )
 
 
 if __name__ == "__main__":
