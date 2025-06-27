9f302b314d99ddd33ac794761c3b73e8de690e9d
Tristan Slominski <tslominski@thousandbrains.org>
2025-06-27 09:21:57 -0500

    style: Add missing from __future__ import annotations

--- tests/unit/habitat_sim_test.py
+++ tests/unit/habitat_sim_test.py
@@ -7,6 +7,7 @@
 # Use of this source code is governed by the MIT
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
+from __future__ import annotations
 
 import pytest
 
