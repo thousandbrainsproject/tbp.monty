44e74d14f0f82799bf9cf4c0e58a75d13f449165
Scott Knudstrup <scottknudstrup@gmail.com>
2025-08-11 20:37:06 -0400

    Update goal_state_selector.py

--- src/tbp/monty/frameworks/models/goal_state_selector.py
+++ src/tbp/monty/frameworks/models/goal_state_selector.py
@@ -26,23 +26,45 @@ class GoalStateSelector:
         self,
         goal_states: Iterable[GoalState | None | Iterable[GoalState | None]],
     ) -> GoalState | None:
-        """Select the best goal state from a list of goal states.
+        """Select the best goal state from zero or more goal states.
+
+        Since goal-state-generators can return `None`, a single goal-state, or an
+        iterable of goal states, pooling the output of several GSGs can result in
+        messy combinations of allowed returned values.
+
+        This function first sanitizes the input goal states by putting all valid
+        goal states into a single flat list.
 
         Args:
-            goal_states: An iterable containing `GoalState` (or `None`) objects.
+            goal_states: A possibly nested iterable of `GoalState` (or `None`) objects.
 
         Returns:
             The goal state with the highest confidence value or `None` if no
             valid goal states were supplied.
         """
-        # Remove "None" goal states and goal states with `use_state` equal to `False`.
-        goal_states = clean_states(goal_states, unusable_ok=False)
+        # Get all items into one flat list.
+        goal_states = flatten(goal_states)
+
+        # Drop `None` goal states.
+        goal_states = [s for s in goal_states if s is not None]
+
+        # Drop states with `use_state` equal to `False`.
+        goal_states = [s for s in goal_states if s.use_state]
+
+        # Quick out if no goal states remain.
         if len(goal_states) == 0:
             return None
 
-        # Sort goal states by confidence
+        # Sort states by confidence value.
         goal_states = sort_states_by_confidence(goal_states, reverse=True)
-        return goal_states[0]
+
+        # Select the goal state with the highest confidence value.
+        highest_confidence_goal_state = goal_states[0]
+
+        # TODO: Figure out how to prevent returning the same location.
+        # TODO: Log the selected goal state.
+
+        return highest_confidence_goal_state
 
 
 def flatten(items: Iterable) -> list:
@@ -57,6 +79,14 @@ def flatten(items: Iterable) -> list:
 
     Returns:
         list: A flat list containing all items from the input iterable.
+
+    Example:
+        >>> flatten([1, 2, [3, "four"], [5, (6, 7)]])
+        [1, 2, 3, "four", 5, 6, 7]
+
+    TODO: Figure out where to put this function. It's used by `GoalStateSelector`, but
+    it's functionality isn't specific to `State` or `GoalState` objects.
+
     """
     if isinstance(items, str):
         return [items]
@@ -71,52 +101,27 @@ def flatten(items: Iterable) -> list:
     return flat
 
 
-def clean_states(
-    states: Iterable[State | None | Iterable[State | None]],
-    unusable_ok: bool = False,
-) -> list[State | None]:
-    """Create a flat list of `State` objects witout `None` values.
-
-    Args:
-        states: A possibly nested iterable of `State` (or `None`) objects.
-        unusable_ok: Whether to include states with a `False` `use_state` attribute in
-            the returned list. Defaults to `False`.
-
-    Returns:
-        A flat list of `State` objects.
-         - May include states with a `False` `use_state` attribute if `unusable_ok`
-           is `True`.
-    """
-    # Flatten states since they may be nested.
-    flat = flatten(states)
-
-    # Remove "None" states.
-    clean = [s for s in flat if s is not None]
-
-    # Remove unusable goal states if not allowed.
-    if not unusable_ok:
-        clean = [s for s in clean if s.use_state]
-
-    return clean
-
-
 def sort_states_by_confidence(
     states: Iterable[State],
     reverse: bool = False,
 ) -> list[State]:
-    """Sort states by confidence.
+    """Sort states according to their confidence values.
 
     Args:
         states: State objects to sort.
         reverse: Whether to sort in reverse order. Defaults to `False`. Set to `True`
-            to have states with higher confidence values first.
+            to have states with highest confidence values first.
 
     Returns:
-        Input states sorted by confidence.
+        Input states sorted by confidence value.
+
+    TODO: Maybe find another home for this function. It's used by `GoalStateSelector`,
+    but it works with regular `State` objects, not just `GoalState` objects, so maybe
+    it belongs in a `states.py` or a utility module.
     """
     confidence = np.array([s.confidence for s in states], dtype=float)
 
-    # Handle None/np.nan confidence values (set them to -np.inf).
+    # Handle None/np.nan confidence values by setting them to -np.inf.
     confidence[np.isnan(confidence)] = -np.inf
 
     # Find sorting indices, possibly reversing them.
