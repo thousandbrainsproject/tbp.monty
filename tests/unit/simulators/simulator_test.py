63945189c1357fbe4bdc72e54b93971942219740
Anna Russo Kennedy <annark@users.noreply.github.com>
2025-08-12 15:04:08 -0700

    Add missing copyright; fix formatting

--- tests/unit/simulators/simulator_test.py
+++ tests/unit/simulators/simulator_test.py
@@ -1,7 +1,17 @@
-import pytest
+# Copyright 2025 Thousand Brains Project
+#
+# Copyright may exist in Contributors' modifications
+# and/or contributions to the work.
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
 import unittest
 
+import pytest
+
 from tbp.monty.simulators.simulator import Simulator
+
 
 class FakeSimulator(Simulator):
     @property
