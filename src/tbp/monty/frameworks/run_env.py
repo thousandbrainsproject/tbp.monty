532701a10f94a472fcdde47fe97d3ae1f8228796
pepedocs <pepedocs@gmail.com>
2025-09-10 18:16:08 +1000

    rebased and fix lint errors

--- src/tbp/monty/frameworks/run_env.py
+++ src/tbp/monty/frameworks/run_env.py
@@ -9,8 +9,8 @@
 # https://opensource.org/licenses/MIT.
 
 import os
-
 from pathlib import Path
+
 
 def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/"):
     """Setup environment variables for Monty.
@@ -18,7 +18,6 @@ def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/"):
     Args:
         monty_logs_dir_default: Default directory for Monty logs.
     """
-
     dir_path = os.getenv("MONTY_LOGS")
     if dir_path is None:
         dir_path = str(Path(monty_logs_dir_default).expanduser())
@@ -26,7 +25,6 @@ def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/"):
         print(f"MONTY_LOGS not set. Using default directory: {dir_path}")
 
     monty_logs_dir = Path(dir_path)
-    
     dir_path = os.getenv("MONTY_MODELS")
     if dir_path is None:
         dir_path = str(Path("~/tbp/results/monty").expanduser() / "pretrained_models")
