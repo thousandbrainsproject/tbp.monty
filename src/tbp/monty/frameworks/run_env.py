98189e4996b54c270e3a823a36566a443f79553c
pepedocs <pepedocs@gmail.com>
2025-09-21 17:57:57 +1000

    fixed lint errors

--- src/tbp/monty/frameworks/run_env.py
+++ src/tbp/monty/frameworks/run_env.py
@@ -12,11 +12,15 @@ import os
 from pathlib import Path
 
 
-def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/", monty_models_dir_default: str = "~/tbp/results/monty/", monty_data_dir_default: str = "~/tbp/data"):
+def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/",
+              monty_models_dir_default: str = "~/tbp/results/monty/",
+              monty_data_dir_default: str = "~/tbp/data"):
     """Setup environment variables for Monty.
 
     Args:
         monty_logs_dir_default: Default directory for Monty logs.
+        monty_models_dir_default: Default directory for Monty pretrained models.
+        monty_data_dir_default: Default directory for Monty experiments data.
     """
     monty_logs_dir = os.getenv("MONTY_LOGS")
     if monty_logs_dir is None:
@@ -26,7 +30,9 @@ def setup_env(monty_logs_dir_default: str = "~/tbp/results/monty/", monty_models
 
     monty_models_dir = os.getenv("MONTY_MODELS")
     if monty_models_dir is None:
-        monty_models_dir = str(Path(monty_models_dir_default).expanduser() / "pretrained_models")
+        monty_models_dir = str(
+            Path(monty_models_dir_default).expanduser() / "pretrained_models"
+        )
         os.environ["MONTY_MODELS"] = monty_models_dir
         print(f"MONTY_MODELS not set. Using default directory: {monty_models_dir}")
 
