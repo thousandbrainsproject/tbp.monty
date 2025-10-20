d8628d4332ed265934a466f8e5cf8f7895c0b0df
Scott Knudstrup <scottcanoe@users.noreply.github.com>
2025-10-20 14:25:43 -0700

    test: add tests for DecayKernel temporal decay

--- tests/unit/frameworks/models/inhibition_of_return_test.py
+++ tests/unit/frameworks/models/inhibition_of_return_test.py
@@ -27,7 +27,7 @@ class DecayKernelTest(unittest.TestCase):
         points = np.array([location + translation * i for i in range(20)])
         spatial_weights = kernel(points)
         for i in range(1, len(points)):
-            self.assertLessEqual(spatial_weights[i], spatial_weights[i - 1])
+            self.assertLess(spatial_weights[i], spatial_weights[i - 1])
 
     def test_kernel_spatial_weight_decays_to_zero_outside_spatial_cutoff(self) -> None:
         location = np.array([1, 2, 3])
@@ -36,3 +36,13 @@ class DecayKernelTest(unittest.TestCase):
         points = np.array([location + translation * i for i in range(20, 100)])
         spatial_weights = kernel(points)
         npt.assert_allclose(spatial_weights, 0.0)
+
+    def test_kernel_temporal_weight_decays_with_time(self) -> None:
+        location = np.array([1, 2, 3])
+        kernel = DecayKernel(location=location)
+        weights = []
+        for _ in range(20):
+            weights.append(kernel(location))
+            kernel.step()
+        for i in range(1, len(weights)):
+            self.assertLess(weights[i], weights[i - 1])
