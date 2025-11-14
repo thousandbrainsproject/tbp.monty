eac022933fda868b4efb781c216f6bc020f6bbec
Tristan Slominski <tslominski@thousandbrains.org>
2025-11-14 09:37:52 -0600

    wip: supervised_pre_training_objects_with_logos_lvl1_monolithic_models config

--- benchmarks/configs/learn_compositional_objects.py
+++ benchmarks/configs/learn_compositional_objects.py
@@ -244,9 +244,7 @@ supervised_pre_training_objects_with_logos_lvl1_monolithic_models.update(
         ),
     ),
     train_env_interface_args=EnvironmentInterfacePerObjectArgs(
-        object_names=get_object_names_by_idx(
-            0, len(OBJECTS_WITH_LOGOS_LVL1), object_list=OBJECTS_WITH_LOGOS_LVL1
-        ),
+        object_names=OBJECTS_WITH_LOGOS_LVL1,
         object_init_sampler=PredefinedObjectInitializer(
             rotations=train_rotations_all,
         ),
