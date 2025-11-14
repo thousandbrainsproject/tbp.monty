a479d10b9cf45a6603c2713c2a949793918b30e0
Tristan Slominski <tslominski@thousandbrains.org>
2025-11-14 09:49:56 -0600

    wip: supervised_pre_training_objects_with_logos_lvl2_comp_models config

--- benchmarks/configs/learn_compositional_objects.py
+++ benchmarks/configs/learn_compositional_objects.py
@@ -314,9 +314,7 @@ supervised_pre_training_objects_with_logos_lvl2_comp_models.update(
         min_lms_match=2,
     ),
     train_env_interface_args=EnvironmentInterfacePerObjectArgs(
-        object_names=get_object_names_by_idx(
-            0, len(OBJECTS_WITH_LOGOS_LVL2), object_list=OBJECTS_WITH_LOGOS_LVL2
-        ),
+        object_names=OBJECTS_WITH_LOGOS_LVL2,
         object_init_sampler=PredefinedObjectInitializer(
             rotations=train_rotations_all,
         ),
