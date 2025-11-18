a3f168915e5bdbd2645028c86217c693be134038
Jason Carver <ut96caarrs@snkmail.com>
2025-11-18 21:07:43 +0100

    fixup! chore: follow ruff rule PTH118

--- benchmarks/make_detailed_follow_up_configs.py
+++ benchmarks/make_detailed_follow_up_configs.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     # Pickle these configs in the follow_ups directory
     follow_up_dir = pathlib.Path(__file__).parent / "configs" / "follow_ups"
     follow_up_name = f"{experiment}_{follow_up_suffix}"
-    file_path = follow_up_dir / follow_up_name + ".pkl"
+    file_path = follow_up_dir / f"{follow_up_name}.pkl"
     with open(file_path, "wb") as f:
         pickle.dump(follow_up_config, f)
 
