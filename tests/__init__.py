7881c581209a953175e7224723e57c8bee55957b
Jeremy Shoemaker <jshoemaker@thousandbrains.org>
2026-02-03 08:53:16 -0600

    test: extract HYDRA_ROOT from test setup
    In test for a separate change, I ran into a situation where I want to take the
    path where the Hydra configs are stored and append more path segments to
    dig down to a part of the configuration. This lead to a long string
    starting with `../../../../` because of where the test was located.
    
    This change sets HYDRA_ROOT in the top level `test/__init__.py` so that
    the tests can all use that instead. It also uses a Path object instead
    of a raw string (though Hydra expects a string, so we have to convert to
    that in the initialize calls).
    
    We're also changing the `hydra.initialize` call to
    `hydra.initialize_config_dir` because the former only takes relative
    paths, and we want to pass an absolute path.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,7 +7,12 @@
 # Use of this source code is governed by the MIT
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
+from pathlib import Path
+
 from tbp.monty.hydra import register_resolvers
 
 # Initialize Hydra resolvers for test configs
 register_resolvers()
+
+# Root directory for Hydra configs
+HYDRA_ROOT = Path(__file__).parents[1] / "src" / "tbp" / "monty" / "conf"
