c17a8bfe1b46e285fd200fe128c628c48c778906
Scott Knudstrup <scottcanoe@users.noreply.github.com>
2026-02-12 14:47:21 -0500

    refactor!: delete min_perc_on_obj, rename and move params.
    - Rename `desired_object_distance` to get_view_distance with used with GetGoodView.
    - Move attrs/params from policy to InformedEnvironmentInterface.
    - Add `desired_object_distance` attribute for SurfacePolicy.

--- tests/integration/positioning_procedures/get_good_view_test.py
+++ tests/integration/positioning_procedures/get_good_view_test.py
@@ -58,11 +58,8 @@ class GetGoodViewTest(unittest.TestCase):
                 exp.pre_epoch()
                 exp.pre_episode()
 
-                policy_cfg = config.test.config["monty_config"]["motor_system_config"][
-                    "motor_system_args"
-                ]["policy"]
-                target_perc_on_target_obj = policy_cfg["good_view_percentage"]
-                target_closest_point = policy_cfg["desired_object_distance"]
+                target_perc_on_target_obj = exp.env_interface.good_view_percentage
+                target_closest_point = exp.env_interface.good_view_distance
 
                 ctx = RuntimeContext(rng=exp.rng)
                 observation = exp.env_interface.step(ctx, first=True)
@@ -111,11 +108,8 @@ class GetGoodViewTest(unittest.TestCase):
                 exp.pre_epoch()
                 exp.pre_episode()
 
-                policy_cfg = config.test.config["monty_config"]["motor_system_config"][
-                    "motor_system_args"
-                ]["policy"]
-                target_perc_on_target_obj = policy_cfg["good_view_percentage"]
-                target_closest_point = policy_cfg["desired_object_distance"]
+                target_perc_on_target_obj = exp.env_interface.good_view_percentage
+                target_closest_point = exp.env_interface.good_view_distance
 
                 ctx = RuntimeContext(rng=exp.rng)
                 observation = exp.env_interface.step(ctx, first=True)
