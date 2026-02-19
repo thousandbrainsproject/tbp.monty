b5cb4da8c94047cc6ee42c4704ae5f4c3fd7f970
Scott Knudstrup <scottcanoe@users.noreply.github.com>
2026-02-18 21:43:54 -0500

    fix: action storage in data_collection_experiments

--- src/tbp/monty/frameworks/experiments/data_collection_experiments.py
+++ src/tbp/monty/frameworks/experiments/data_collection_experiments.py
@@ -72,14 +72,11 @@ class DataCollectionExperiment(MontyObjectRecognitionExperiment):
         self.model.sensor_modules[0].processed_obs[-1]["object"] = (
             self.env_interface.primary_target["object"]
         )
-        self.model.sensor_modules[0].processed_obs[-1]["action"] = (
-            None
-            if self.model.motor_system._policy.actions is None
-            else (
-                f"{self.model.motor_system._policy.actions[0].agent_id}."
-                f"{self.model.motor_system._policy.actions[0].name}"
-            )
-        )
+        action_strings = [
+            f"{action.agent_id}.{action.name}"
+            for action in self.model.motor_system._policy.actions
+        ]
+        self.model.sensor_modules[0].processed_obs[-1]["actions"] = action_strings
         # Only include observations coming right before a move_tangentially action
         if step > 0 and (
             not self.model.motor_system._policy.actions
