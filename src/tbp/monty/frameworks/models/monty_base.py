eb2fb869dd831bb5f995d048eed6a3f0cfdb488b
Tristan Slominski <tslominski@thousandbrains.org>
2026-02-26 13:16:32 -0600

    fix: do not decorate None if no Percept from sensor module

--- src/tbp/monty/frameworks/models/monty_base.py
+++ src/tbp/monty/frameworks/models/monty_base.py
@@ -151,7 +151,7 @@ class MontyBase(Monty):
             raw_obs = self.get_observations(observation, sensor_module.sensor_module_id)
             sensor_module.update_state(self.get_agent_state())
             sm_output = sensor_module.step(ctx, raw_obs)
-            if self.is_motor_only_step:
+            if sm_output is not None and self.is_motor_only_step:
                 sm_output.use_state = False
             sensor_module_outputs.append(sm_output)
         # Aggregate LM outputs here to be input to higher level LM at next step
