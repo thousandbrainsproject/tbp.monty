e02bad90d68b28dcdc0231a74330168e11ea3cc7
Hojae Lee <hlee@thousandbrains.org>
2026-03-13 02:56:56 +0000

    chore: test/graph_learning/feature_pred_off_object_train

--- src/tbp/monty/conf/compare_snapshots.py
+++ src/tbp/monty/conf/compare_snapshots.py
@@ -129,6 +129,7 @@ RUNS = [
     "test/graph_learning/feature_pred",
     "test/graph_learning/feature_pred_time_out",
     "test/graph_learning/feature_pred_off_object",
+    "test/graph_learning/feature_pred_off_object_train",
     "test/graph_learning/ppf_pred",
 ]
 
