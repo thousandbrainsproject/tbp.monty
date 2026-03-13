132d1266b7aa53a418ec69d0e20adb0304ce0dbf
Hojae Lee <hlee@thousandbrains.org>
2026-03-13 05:56:47 +0000

    chore: test/hierarchy/two_lms_eval

--- src/tbp/monty/conf/compare_snapshots.py
+++ src/tbp/monty/conf/compare_snapshots.py
@@ -141,6 +141,7 @@ RUNS = [
     "test/profile/base",
     "test/hierarchy/base",
     "test/hierarchy/two_lms_constrained",
+    "test/hierarchy/two_lms_eval",
     "test/integration/positioning_procedures/get_good_view/base",
     "test/integration/positioning_procedures/get_good_view/dist_agent_too_far_away",
     "test/integration/positioning_procedures/get_good_view/multi_object_target_not_visible",
