f0cbab20a986ae115348c3d328d4332fb76af8ac
Hojae Lee <hlee@thousandbrains.org>
2026-06-01 11:40:57 -0700

    chore: remove is_orthogonal check

--- src/tbp/monty/frameworks/utils/sensor_processing.py
+++ src/tbp/monty/frameworks/utils/sensor_processing.py
@@ -155,9 +155,6 @@ def directional_curvature(
             movement_direction does not lie in the plane spanned by pc1_dir
             and pc2_dir.
     """
-    if not is_orthogonal(pc1_dir, pc2_dir):
-        raise ValueError("The pc1_dir and pc2_dir must be orthogonal.")
-
     if not (is_unit_vector(pc1_dir) and is_unit_vector(pc2_dir)):
         raise ValueError("The pc1_dir and pc2_dir must be unit vectors.")
 
