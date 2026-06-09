6d9f42358b05fc091f64090e04f2522870fe51dc
Jeremy Shoemaker <jshoemaker@thousandbrains.org>
2026-06-09 14:01:01 -0500

    tests: update `test/mujoco/integration/__init__.py`

--- tests/mujoco/integration/__init__.py
+++ tests/mujoco/integration/__init__.py
@@ -6,3 +6,9 @@
 # Use of this source code is governed by the MIT
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
+
+import tempfile
+
+from tbp.monty.frameworks.run_env import setup_env
+
+setup_env(monty_logs_dir_default=tempfile.mkdtemp())
