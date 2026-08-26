04c55c826ee932d2e1bde30f2048df458095ecb2
Dale Schumacher <dschumacher@thousandbrains.org>
2026-08-26 14:54:20 -0600

    chore: removed TODO

--- src/tbp/monty/frameworks/experiments/object_recognition_experiments.py
+++ src/tbp/monty/frameworks/experiments/object_recognition_experiments.py
@@ -155,7 +155,6 @@ class MontyObjectRecognitionExperiment(MontyExperiment):
                 #       so the experiment can set max steps based on that knowledge
                 #       alone.
                 self.model.set_is_done()
-                # TODO: `MontyBase.set_is_done()` is not defined, use `set_done()`?
                 return step
 
             if self._recognition_complete(step):
