f78aebfe43d7c3cdbc5c046c3df88746f05233a6
Dale Schumacher <dschumacher@thousandbrains.org>
2026-09-08 14:49:38 -0600

    refactor!: introduce `ObjectRecognition` Recognition Policy

--- src/tbp/monty/experiment/recognition_policy.py
+++ src/tbp/monty/experiment/recognition_policy.py
@@ -8,6 +8,7 @@
 # https://opensource.org/licenses/MIT.
 from __future__ import annotations
 
+import logging
 import math
 from dataclasses import dataclass
 from typing import Protocol
@@ -26,6 +27,8 @@ __all__ = [
     "RecognitionPolicy",
     "RecognitionResult",
 ]
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -189,6 +192,58 @@ class NaiveScan(RecognitionPolicy):
         self: Self, model: MontyBase, count: RecognitionCounter
     ) -> RecognitionResult:
         if count.step >= self._step_limit:
+            return RecognitionResult(is_done=True)
+
+        return RecognitionResult(is_done=model.is_done)
+
+
+class ObjectRecognition(RecognitionPolicy):
+    """Determine terminal conditions for object recognition experiments.
+
+    Terminal conditions include:
+    - `model.is_done`
+    - `count.steps >= count.max_steps`
+    - `count.steps >= count.max_total_steps`
+    """
+
+    _max_steps: int
+    """The maximum number of matching steps before terminating."""
+
+    _max_total_steps: int
+    """The maximum total number of steps before terminating."""
+
+    def __init__(self: Self, max_steps: int, max_total_steps: int) -> None:
+        """Initialize the policy.
+
+        Args:
+            max_steps: The maximum number of matching steps before terminating.
+            max_total_steps: The maximum total number of steps before terminating.
+
+        Raises:
+            ValueError: If `max_steps` or `max_total_steps` are not positive.
+        """
+        if max_steps <= 0:
+            raise ValueError("max_steps must be positive")
+
+        if max_total_steps <= 0:
+            raise ValueError("max_total_steps must be positive")
+
+        self._max_steps = max_steps
+        self._max_total_steps = max_total_steps
+
+    def __call__(
+        self: Self, model: MontyBase, count: RecognitionCounter
+    ) -> RecognitionResult:
+        if (not model.is_exploring) and (model.matching_steps >= self._max_steps):
+            logger.info(f"Terminated due to maximum matching steps : {self._max_steps}")
+            model.deal_with_time_out()
+            return RecognitionResult(is_done=True)
+
+        if count.step >= self._max_total_steps:
+            logger.info(
+                f"Terminated due to maximum episode steps : {self._max_total_steps}"
+            )
+            model.deal_with_time_out()
             return RecognitionResult(is_done=True)
 
         return RecognitionResult(is_done=model.is_done)
