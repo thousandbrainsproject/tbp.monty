579b1ef308a8ebbf344d0f0c2b98cd07463358c1
Dale Schumacher <dschumacher@thousandbrains.org>
2026-09-11 14:46:26 -0600

    wip: policy uses `max_train_steps` and `max_eval_steps`

--- src/tbp/monty/experiment/recognition_policy.py
+++ src/tbp/monty/experiment/recognition_policy.py
@@ -15,6 +15,7 @@ from typing import Protocol
 
 from typing_extensions import Self
 
+from tbp.monty.frameworks.experiments.mode import ExperimentMode
 from tbp.monty.frameworks.models.monty_base import MontyBase
 
 __all__ = [
@@ -39,8 +40,8 @@ class RecognitionCounter:
     step: int = 0
     """The current step number."""
 
-    max_steps: int = 0
-    """The maximum number of steps before terminating the episode."""
+    mode: ExperimentMode = ExperimentMode.EVAL
+    """The stepping mode (traning or evaluation)."""
 
 
 @dataclass
@@ -205,33 +206,57 @@ class ObjectRecognition(RecognitionPolicy):
     """Determine terminal conditions for object recognition experiments.
 
     Terminal conditions include:
-    - `model.is_done`
-    - `model.matching_steps >= count.max_steps`
+    - `model.matching_steps >= {max_train_steps | max_eval_steps}`
     - `count.step >= max_total_steps`
+    - `model.is_done`
     """
+
+    _max_train_steps: int
+    """The maximum steps to take in training mode."""
+
+    _max_eval_steps: int
+    """The maximum steps to take in evaluation mode."""
 
     _max_total_steps: int
     """The maximum total number of steps before terminating."""
 
-    def __init__(self: Self, max_total_steps: int) -> None:
+    def __init__(
+        self: Self, max_train_steps: int, max_eval_steps: int, max_total_steps: int
+    ) -> None:
         """Initialize the policy.
 
         Args:
+            max_train_steps: The maximum steps to take in training mode.
+            max_eval_steps: The maximum steps to take in evaluation mode.
             max_total_steps: The maximum total number of steps before terminating.
 
         Raises:
-            ValueError: If `max_total_steps` is not positive.
+            ValueError: If `max_train_steps`, `max_eval_steps`, or `max_total_steps`
+                are not positive.
         """
+        if max_train_steps <= 0:
+            raise ValueError("max_train_steps must be positive")
+
+        if max_eval_steps <= 0:
+            raise ValueError("max_eval_steps must be positive")
+
         if max_total_steps <= 0:
             raise ValueError("max_total_steps must be positive")
 
+        self._max_train_steps = max_train_steps
+        self._max_eval_steps = max_eval_steps
         self._max_total_steps = max_total_steps
 
     def __call__(
         self: Self, model: MontyBase, count: RecognitionCounter
     ) -> RecognitionResult:
-        if (not model.is_exploring) and (model.matching_steps >= count.max_steps):
-            logger.info(f"Terminated due to maximum matching steps : {count.max_steps}")
+        max_steps = (
+            self._max_train_steps
+            if count.mode is ExperimentMode.TRAIN
+            else self._max_eval_steps
+        )
+        if (not model.is_exploring) and (model.matching_steps >= max_steps):
+            logger.info(f"Terminated due to maximum matching steps : {max_steps}")
             model.deal_with_time_out()
             return RecognitionResult(is_done=True)
 
